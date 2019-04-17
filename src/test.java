b21712a9a1ee62c016d05454e36c9302c28e83df
makloao <1332553530@qq.com>
2019-04-17 12:18:12 +0800

    Mirror,操作给定的二叉树，将其变换为源二叉树的镜像。

---
+++ src/test.java
@@ -0,0 +1,17 @@
+public class test {
+
+/**
+ * @program: offer
+ *
+ * @description:
+ *
+ * @author: makaloo
+ *
+ * @create: 2019-04-14 21:10
+ **/
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(~i+1);
+
+    }
+}
