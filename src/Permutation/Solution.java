c5fb4b4c6b5ca33088ed58e05ed7b9576269a8e1
makloao <1332553530@qq.com>
2019-05-16 09:47:12 +0800

    Permutation，输入字符串，输出其全排列（不能有重复），重复元素好解决，每次加入前判断list是否有就行了，关键是递归的思路，依次交换首元素和所有元素（包括首元素），然后固定交换后的首元素，对后面的元素递归交换，递归结束是还没固定的首元素就是末尾元素，这时把当前数组加入到list，难点是每次递归结束要把交换的元素复位，保证递归外层不会收到影响

---
+++ src/Permutation/Solution.java
@@ -0,0 +1,47 @@
+package Permutation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+
+/**
+ * @program: offer
+ *
+ * @description: 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+ *
+ * @author: makaloo
+ *
+ * @create: 2019-05-15 08:34
+ **/
+    // 算法思想：采用递归，首先依次交换首元素和后面的元素，例如abc得bac，cba，然后固定第一个元素，对后面的元素递归交换，得acb，bca，cab
+    // 递归结束条件：i = length - 1（交换到最后一个元素），递归结束后把结果存入list
+    // 难点：每次递归结束后都要把数组恢复原来的状态
+    public ArrayList<String> Permutation(String str) {
+        ArrayList<String> list = new ArrayList<>();
+        if (str == null || str.length() == 0) return list;
+        Permutation(str.toCharArray(), 0, list);
+        Collections.sort(list);
+        return list;
+    }
+
+    private void Permutation(char[] chars,int i, ArrayList<String> list) {
+        if (i == chars.length - 1) {
+            String result = String.valueOf(chars);
+            if (!list.contains(result))
+                list.add(result);
+        } else {
+            for (int j = i; j < chars.length; j++) {
+                swap(chars, i, j);
+                Permutation(chars, i + 1, list);
+                swap(chars, j, i); // 使数组顺序回到递归前的状态，这样不会影响外部的遍历顺序
+            }
+        }
+    }
+
+    private void swap(char[] cs, int i, int j) {
+        char temp = cs[i];
+        cs[i] = cs[j];
+        cs[j] = temp;
+    }
+}
