b36db2ad6ac336394faca787d1e134bdfff696ef
makloao <1332553530@qq.com>
2019-05-21 08:53:13 +0800

    NumberOf1Between1AndN_Solution,求出任意非负整数区间中1出现的次数，思路就是从每一位出现1的可能性去考虑，找出规律

---
+++ src/NumberOf1Between1AndN_Solution/NumberOf1Between1AndNTest.java
@@ -0,0 +1,18 @@
+package NumberOf1Between1AndN_Solution;
+
+public class NumberOf1Between1AndNTest {
+
+/**
+ * @program: offer
+ *
+ * @description:
+ *
+ * @author: makaloo
+ *
+ * @create: 2019-05-21 08:45
+ **/
+    public static void main(String... args) {
+        Solution s = new Solution();
+        System.out.println(s.NumberOf1Between1AndN_Solution(10));
+    }
+}
