0a4fe485b0143531a698cb38c628438c68ade42a
makloao <1332553530@qq.com>
2019-05-24 08:19:06 +0800

    GetUglyNumber_Solution，把只包含质因子2、3和5的数称作丑数，1当做是第一个丑数。求按从小到大的顺序的第N个丑数，关键：找到丑数的规律，它们是之前丑数*2或*3或*5所得到的

---
+++ src/GetUglyNumber_Solution/GetUglyNumberTest.java
@@ -0,0 +1,9 @@
+package GetUglyNumber_Solution;
+
+public class GetUglyNumberTest {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.GetUglyNumber_Solution(7));
+    }
+}
